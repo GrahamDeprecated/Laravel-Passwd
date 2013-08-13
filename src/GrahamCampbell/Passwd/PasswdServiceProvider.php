c6aa6d28a736813fdc7c0b7338516ff5a6181e1b
Graham Campbell <graham@mineuk.com>
2013-08-13 22:19:33 +0100

    Fixed spacing

--- src/GrahamCampbell/Passwd/PasswdServiceProvider.php
+++ src/GrahamCampbell/Passwd/PasswdServiceProvider.php
@@ -4,39 +4,39 @@ use Illuminate\Support\ServiceProvider;
 
 class PasswdServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot() {
-		$this->package('graham-campbell/passwd');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+        $this->package('graham-campbell/passwd');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register() {
-		$this->app['passwd'] = $this->app->share(function($app) {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        $this->app['passwd'] = $this->app->share(function($app) {
             return new GrahamCampbell\Passwd\Classes\Passwd;
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides() {
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return array();
+    }
 }
